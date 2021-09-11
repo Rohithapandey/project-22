d376ab3e3c045030ca87b93d2fff628e8d895d09
Rohithapandey <86673759+Rohithapandey@users.noreply.github.com>
2021-09-11 20:58:26 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,20 @@
+class rope{
+	constructor(body1,body2,pointA,pointB)
+   { 
+	this.pointA = pointA ;
+    this.pointB = pointB ;
+
+	var option = {
+         bodyA : body1,
+         bodyB : body2,
+         pointB :{x:pointA ,y:pointB}
+		
+	}
+	//create rope constraint here
+
+	}
+
+
+
+    //create display() here 
+}
